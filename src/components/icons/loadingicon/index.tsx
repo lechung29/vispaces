aeb0ab3d291377c644bf63fec85794ba83d9af22
Killian.Le <killian.le@avepoint.com>
2025-03-31 11:46:25 +0700

    Update submit button component logic and style

--- src/components/icons/loadingicon/index.tsx
+++ src/components/icons/loadingicon/index.tsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import React from "react";
 import "./index.scss";
 
@@ -5,18 +7,23 @@ export interface IIconProps {
     size?: number;
     color?: string;
     withLoadingText?: boolean;
+    fontWeight?: string
 }
 
-const LoadingIcon: React.FunctionComponent<IIconProps> = (props) => {
-    const { size, color, withLoadingText = false } = props;
+const LoadingIcon: React.FunctionComponent<Required<IIconProps>> = (props) => {
+    const { size, color, withLoadingText, fontWeight } = props;
     return (
-        <div className="g-loading-icon-section"> 
-            <div className="dot-spinner" style={{ "--uib-color": color, "--uib-size": `${size}px` } as React.CSSProperties}>
-                {Array.from({length: 8}).map((_item, index) => (
+        <div className="g-loading-icon-section">
+            <div className="dot-spinner" style={{ "--button-color": color, "--uib-size": `${size}px` } as React.CSSProperties}>
+                {Array.from({ length: 8 }).map((_item, index) => (
                     <div key={index} className="dot-spinner__dot"></div>
                 ))}
             </div>
-            {withLoadingText && <div className="loading-text" style={{"--uib-color": color } as React.CSSProperties}>Please waiting . . .</div>}
+            {withLoadingText && (
+                <div className="loading-text" style={{ "--button-color": color, "--font-weight": fontWeight } as React.CSSProperties}>
+                    Please waiting . . .
+                </div>
+            )}
         </div>
     );
 };
