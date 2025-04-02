14b9cc26ed309a52757789b584160775f270c439
Killian.Le <killian.le@avepoint.com>
2025-04-02 17:55:30 +0700

    Update login page and auth layout - remove tailwind classname and use scss file

--- src/layouts/auth-layout/index.tsx
+++ src/layouts/auth-layout/index.tsx
@@ -2,13 +2,12 @@ import { Outlet } from "react-router-dom";
 import { Notification } from "@/components/notification";
 import { useAppSelector } from "@/redux/store/store";
 import { notificationBarState } from "@/redux/reducers";
-import { motion } from 'framer-motion';
 import "./index.scss"
 
 export const AuthLayout: React.FunctionComponent = () => {
     const { isShow, message, type, duration } = useAppSelector(notificationBarState)
-    return <motion.div className="common-auth-container relative w-screen h-screen overflow-hidden flex items-center justify-center py-3 px-2">
+    return <section className="g-common-auth-container">
         <Outlet />
         {isShow && <Notification duration={duration} isShow={isShow} message={message} type={type}/>}
-    </motion.div>
+    </section>
 };
