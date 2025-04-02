2ab67e1a77bd46d934a163c9ce0ce65e15ce8449
killian.le <chungle924@gmail.com>
2025-04-03 01:09:52 +0700

    Update change theme logic

--- src/pages/authentication/login/Login.tsx
+++ src/pages/authentication/login/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './index.scss';
 import { useNavigate } from 'react-router-dom';
 import { motion, stagger, useAnimate } from 'framer-motion';
