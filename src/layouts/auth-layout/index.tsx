97a41161334123d9e343f3598befee303098e760
killian.le <chungle924@gmail.com>
2025-04-03 02:13:02 +0700

    Update auth layout scrollbar

--- src/layouts/auth-layout/index.tsx
+++ src/layouts/auth-layout/index.tsx
@@ -1,13 +1,30 @@
+/** @format */
+
 import { Outlet } from "react-router-dom";
 import { Notification } from "@/components/notification";
 import { useAppSelector } from "@/redux/store/store";
 import { notificationBarState } from "@/redux/reducers";
-import "./index.scss"
+import "./index.scss";
+import { ScrollArea } from "@radix-ui/themes";
 
 export const AuthLayout: React.FunctionComponent = () => {
-    const { isShow, message, type, duration } = useAppSelector(notificationBarState)
-    return <section className="g-common-auth-container">
-        <Outlet />
-        {isShow && <Notification duration={duration} isShow={isShow} message={message} type={type}/>}
-    </section>
+    const { isShow, message, type, duration } = useAppSelector(notificationBarState);
+    return (
+        <ScrollArea 
+            className="g-auth-layout-scrollbar"
+            size="1" 
+            type="scroll" 
+            scrollbars="both"
+            scrollHideDelay={1000}
+            style={{
+                height: "100vh", 
+                width: "100vw",
+            }}
+        >
+            <section className="g-common-auth-container">
+                <Outlet />
+                {isShow && <Notification duration={duration} isShow={isShow} message={message} type={type} />}
+            </section>
+        </ScrollArea>
+    );
 };
